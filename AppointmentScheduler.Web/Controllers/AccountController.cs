2d290fb7b238b4ef38fd0a0a46ba310e9662ed94
erastyk28@gmail.com <erastyk28@gmail.com>
2022-04-17 18:43:53 +0300

    e

---
+++ AppointmentScheduler.Web/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduler.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        ////private readonly ApplicationDbContext _db;
+        /*public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        */
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
